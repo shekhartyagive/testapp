1a335076034c15c1c6c4dc6c591adbf6e13b4ea8
shekhar tyagi <shekhartyagi@virtualemployee.com>
2019-08-06 18:09:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Circle CI
         </a>
       </header>
     </div>
